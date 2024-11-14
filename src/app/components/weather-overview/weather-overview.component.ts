cec4b05fee85878b266aedda5aec69e385dc401a
HernanCerillano <cerillanohernan010@gmail.com>
2024-11-14 16:23:10 -0300

    Modificacion Wheater View
    Toque el css, hmtl y ts:
    ahora se ve de forma ordenada el clima del dia y de los siguientes dias de la semana.

--- src/app/components/weather-overview/weather-overview.component.ts
+++ src/app/components/weather-overview/weather-overview.component.ts
@@ -25,18 +25,18 @@ export class WeatherOverviewComponent implements OnInit {
   weatherService = inject(WeatherService);
 
   ngOnInit() {
-    this.route.queryParams.subscribe(params => { //tuve que hacerlo con queryparams porque me tiraba un error en +params
+    this.route.queryParams.subscribe(params => {
       this.city = params['city'] || null;
       this.lat = params['lat'] ? +params['lat'] : null;
       this.lon = params['lon'] ? +params['lon'] : null;
-      
+
       if (this.lat !== null && this.lon !== null) {
         this.weatherService.getWeatherByCoordinates(this.lat, this.lon).subscribe(data => {
           this.weatherData = data;
         });
 
         this.weatherService.getForecastByCoordinates(this.lat, this.lon).subscribe(data => {
-          this.forecastData = data;
+          this.forecastData = this.getDailyForecast(data.list);
         });
       } else if (this.city) {
         this.weatherService.getWeatherByCity(this.city).subscribe(data => {
@@ -44,18 +44,27 @@ export class WeatherOverviewComponent implements OnInit {
         });
 
         this.weatherService.getForecastByCity(this.city).subscribe(data => {
-          this.forecastData = data;  
-          console.log(this.forecastData);
-           
+          this.forecastData = this.getDailyForecast(data.list);
         });
       }
-      
     });
+  }
+
+  // Filtra el pronóstico para obtener solo un dato por día
+  getDailyForecast(forecastList: any[]): any[] {
+    const dailyForecast = forecastList.reduce((acc, curr) => {
+      const date = new Date(curr.dt_txt).toLocaleDateString("en-US", { weekday: 'short', month: 'short', day: 'numeric' });
+      if (!acc.find((item: any) => item.date === date)) {
+        acc.push({ ...curr, date });
+      }
+      return acc;
+    }, []);
+    return dailyForecast;
   }
 
 
   convertUnixTime(unixTime: number, timezoneOffset: number): string { //Calculo para transformar el sunrise y sunset en información legible
-    const date = new Date((unixTime + timezoneOffset) * 1000); 
+    const date = new Date((unixTime + timezoneOffset) * 1000);
     return new Intl.DateTimeFormat('en-US', {
       hour: '2-digit',
       minute: '2-digit',
@@ -66,17 +75,17 @@ export class WeatherOverviewComponent implements OnInit {
 
   formatForecastDate(dt_txt: string): string {
     const date = new Date(dt_txt);
-  
-    return new Intl.DateTimeFormat('en-US', {
-      weekday: 'short', 
-      year: 'numeric', 
-      month: 'short', 
-      day: 'numeric', 
-      hour: '2-digit', 
-      minute: '2-digit', 
-      hour12: true 
-    }).format(date);
-  } 
 
- 
+    return new Intl.DateTimeFormat('en-US', {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true
+    }).format(date);
+  }
+
+
 }
