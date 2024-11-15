4f0b5c0e3fbd92c6e86d68a5bcc22c3c0148a16d
unknown <cerillanohernan010@gmail.com>
2024-11-15 09:57:36 -0300

    ultimo

--- src/app/pages/overview-page/overview-page.component.ts
+++ src/app/pages/overview-page/overview-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { WeatherOverviewComponent } from "../../components/weather-overview/weather-overview.component";
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-overview-page',
   standalone: true,
-  imports: [WeatherOverviewComponent],
+  imports: [WeatherOverviewComponent,FooterComponent],
   templateUrl: './overview-page.component.html',
   styleUrl: './overview-page.component.css'
 })
